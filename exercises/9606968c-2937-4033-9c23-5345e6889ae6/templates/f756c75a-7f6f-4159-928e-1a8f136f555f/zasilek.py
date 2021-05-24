b336c2b2993ce1a37a32f908d3b43ce6d46064cb
Thomas Naprawski <tnaprawski@gmail.com>
2021-05-24 22:49:45 +0100

    updated file exercises/9606968c-2937-4033-9c23-5345e6889ae6/templates/f756c75a-7f6f-4159-928e-1a8f136f555f/zasilek.py

--- exercises/9606968c-2937-4033-9c23-5345e6889ae6/templates/f756c75a-7f6f-4159-928e-1a8f136f555f/zasilek.py
+++ exercises/9606968c-2937-4033-9c23-5345e6889ae6/templates/f756c75a-7f6f-4159-928e-1a8f136f555f/zasilek.py
@@ -2,5 +2,5 @@ dzieci = input("Liczba dzieci: ")
 dochod = input("Dochód na członka: ") 
 nieplnospr = input("Niepełnosprawne: ")
 
-[Tutaj wpisz swoj kod wyliczajacy sume zasiłku, przykladowo:]
+[Tutaj wpisz swoj kod wyliczajacy sume zasiłku. Nie zapomnij użyć instrukcji sprawdzającej]
 

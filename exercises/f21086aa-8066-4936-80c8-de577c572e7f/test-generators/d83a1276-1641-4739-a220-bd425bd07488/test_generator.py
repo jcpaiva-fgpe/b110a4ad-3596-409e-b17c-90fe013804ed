308a36623cfcc388522025e769ca3a304271c8ed
Thomas Naprawski <tnaprawski@gmail.com>
2021-05-26 17:57:33 +0100

    created file exercises/f21086aa-8066-4936-80c8-de577c572e7f/test-generators/d83a1276-1641-4739-a220-bd425bd07488/test_generator.py

---
+++ exercises/f21086aa-8066-4936-80c8-de577c572e7f/test-generators/d83a1276-1641-4739-a220-bd425bd07488/test_generator.py
@@ -0,0 +1,26 @@
+#printf $solutions/sol.py"\n10\n" | python $file
+
+from random import choice as ch
+
+li = ['', 'A', 'B', 'C', 'E', 'D', 'X', 'Z']
+
+f = open(input())
+source = f.read()
+exec(source)
+
+
+nr_of_tests = int(input())
+
+for i in range(0, nr_of_tests):
+    word_li = [ch(li)+ch(li) for i in range(len(li)+1)]
+    word_li = ' '.join(word_li)
+    
+    f = open("tests/input" + str(i+1) + ".txt", "w+")
+    f.write("%s\n" % (word_li))
+    f.close()
+
+    out = niepowtarzalne(word_li)
+
+    f = open("tests/output" + str(i+1) + ".txt", "w+")
+    f.write('\n'.join(out))
+    f.close()

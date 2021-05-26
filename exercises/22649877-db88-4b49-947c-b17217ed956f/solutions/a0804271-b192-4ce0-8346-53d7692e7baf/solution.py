fa79f788fd90d1dba0e855a75bef504b6770e2b8
Thomas Naprawski <tnaprawski@gmail.com>
2021-05-26 09:09:43 +0100

    updated file exercises/22649877-db88-4b49-947c-b17217ed956f/solutions/a0804271-b192-4ce0-8346-53d7692e7baf/solution.py

--- exercises/22649877-db88-4b49-947c-b17217ed956f/solutions/a0804271-b192-4ce0-8346-53d7692e7baf/solution.py
+++ exercises/22649877-db88-4b49-947c-b17217ed956f/solutions/a0804271-b192-4ce0-8346-53d7692e7baf/solution.py
@@ -1,3 +1,6 @@
 napis = input().lower().split()
-d = {napis[i] : j.count('a') for i, j in enumerate(napis)} 
-print(max(d, key = d.get))
+d = {napis[i]: j.count('a') for i, j in enumerate(napis)} 
+try:
+    print(max(d, key = d.get))
+except ValueError:
+    print()

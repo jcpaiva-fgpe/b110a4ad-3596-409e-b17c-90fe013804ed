54f4e19e016b454e0ba4d748bafecf63ca3a8e13
Thomas Naprawski <tnaprawski@gmail.com>
2021-05-26 12:28:39 +0100

    updated file exercises/687190d0-81a5-49f2-9035-481e69b6a48c/solutions/b938d036-9862-4d38-8f4d-a2c26159694f/rozwiazanie.py

--- exercises/687190d0-81a5-49f2-9035-481e69b6a48c/solutions/b938d036-9862-4d38-8f4d-a2c26159694f/rozwiazanie.py
+++ exercises/687190d0-81a5-49f2-9035-481e69b6a48c/solutions/b938d036-9862-4d38-8f4d-a2c26159694f/rozwiazanie.py
@@ -1,4 +1,10 @@
 def niepowtarzalne(st):
+    
+    try:
+        st = str(st)
+    except TypeError:
+        return 'blad wprowadzenia'
+
     st = st.lower().split()
     res = {i : st.count(i) for i in set(st) if st.count(i)>1}
-    return(f"zawiera {list(res.keys())}" if len(res) != 0 else "nie zawiera")
+    return f"zawiera {list(res.keys())}" if len(res) != 0 else "nie zawiera"

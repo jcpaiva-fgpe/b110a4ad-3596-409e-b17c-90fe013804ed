2232ec007ac817d06a959ac4ef5493e364dffac1
Thomas Naprawski <tnaprawski@gmail.com>
2021-05-26 01:56:23 +0100

    updated file exercises/687190d0-81a5-49f2-9035-481e69b6a48c/test-generators/7e49b622-2fa3-4da6-8061-e6644ccda49b/testgen.py

--- exercises/687190d0-81a5-49f2-9035-481e69b6a48c/test-generators/7e49b622-2fa3-4da6-8061-e6644ccda49b/testgen.py
+++ exercises/687190d0-81a5-49f2-9035-481e69b6a48c/test-generators/7e49b622-2fa3-4da6-8061-e6644ccda49b/testgen.py
@@ -15,12 +15,12 @@ for i in range(0, nr_of_tests):
     word_li = [ch(li)+ch(li) for i in range(len(li)+1)]
     word_li = ' '.join(word_li)
     
-    f = open("tests/in" + str(i+1) + ".txt", "w+")
+    f = open("tests/input" + str(i+1) + ".txt", "w+")
     f.write("%s\n" % (word_li))
     f.close()
 
     out = niepowtarzalne(word_li)
 
-    f = open("tests/out" + str(i+1) + ".txt", "w+")
+    f = open("tests/output" + str(i+1) + ".txt", "w+")
     f.write('\n'.join(out))
     f.close()

fdc48fdb9e7ee6a4fbedce8dfeb5dc8728a8ef4f
Thomas Naprawski <tnaprawski@gmail.com>
2021-05-28 16:02:43 +0100

    updated file exercises/568574e3-a365-40c5-97f8-50133f9204c3/solutions/ca862081-2fdb-47d4-a60b-014e36b3336a/sol.py

--- exercises/568574e3-a365-40c5-97f8-50133f9204c3/solutions/ca862081-2fdb-47d4-a60b-014e36b3336a/sol.py
+++ exercises/568574e3-a365-40c5-97f8-50133f9204c3/solutions/ca862081-2fdb-47d4-a60b-014e36b3336a/sol.py
@@ -1,4 +1,4 @@
 in1 = input('Podaj pierwszy napis: ').split()
-in2 = input('Podaj pierwszy napis: ').split()
+in2 = input('Podaj drugi napis: ').split()
 
 print('Takie same wyrazy to:', *list(set(in1) & set(in2)))

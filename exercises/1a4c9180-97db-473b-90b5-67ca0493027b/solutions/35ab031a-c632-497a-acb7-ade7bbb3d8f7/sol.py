5c1991396b00bf2a2692401fea9db423ca769164
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-05 19:49:42 +0100

    Removed exercises/1a4c9180-97db-473b-90b5-67ca0493027b/solutions/35ab031a-c632-497a-acb7-ade7bbb3d8f7/sol.py

--- exercises/1a4c9180-97db-473b-90b5-67ca0493027b/solutions/35ab031a-c632-497a-acb7-ade7bbb3d8f7/sol.py
+++
@@ -1,9 +0,0 @@
-litera = 'A'
-while True:
-    print(f"Podaj slowo na litere {litera}:")
-    slowo = input().upper()
-    if slowo[0] == litera:
-        litera = slowo[-1]
-    else:
-        print("Przegrales.")
-        break

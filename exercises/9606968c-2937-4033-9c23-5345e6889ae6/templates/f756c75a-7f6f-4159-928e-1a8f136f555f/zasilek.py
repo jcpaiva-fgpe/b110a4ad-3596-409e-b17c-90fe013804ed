cada3b938a11d1a492db696e9478296f1f62cb16
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-05 19:51:29 +0100

    Removed exercises/9606968c-2937-4033-9c23-5345e6889ae6/templates/f756c75a-7f6f-4159-928e-1a8f136f555f/zasilek.py

--- exercises/9606968c-2937-4033-9c23-5345e6889ae6/templates/f756c75a-7f6f-4159-928e-1a8f136f555f/zasilek.py
+++
@@ -1,8 +0,0 @@
-dzieci = input("Liczba dzieci: ")
-dochod = input("Dochód na członka: ") 
-nieplnospr = input("Niepełnosprawne: ")
-
-#[Tutaj wpisz swój kod wyliczajacy sume zasiłku.] 
-#[Nie zapomnij użyć instrukcji sprawdzającej.] 
-#[Wyświetl tekst.]
-

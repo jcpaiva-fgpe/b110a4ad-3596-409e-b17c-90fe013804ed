9a60c9c30a52b87f480258eda93abb8922c6519f
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-05 19:50:13 +0100

    Removed exercises/15ff6ad0-8fa6-4b1c-b000-5af7c8e03f33/solutions/6070e115-9f54-4d72-96b4-de6b214a8618/sol.py

--- exercises/15ff6ad0-8fa6-4b1c-b000-5af7c8e03f33/solutions/6070e115-9f54-4d72-96b4-de6b214a8618/sol.py
+++
@@ -1,7 +0,0 @@
-print('Podaj dwa rozne napisy dzielac je przecinkiem:')
-zdania = input().split(",")
-
-in1 = zdania[0].split() 
-in2 = zdania[1].split()
-
-print('Takie same wyrazy to:', *list(set(in1) & set(in2)))

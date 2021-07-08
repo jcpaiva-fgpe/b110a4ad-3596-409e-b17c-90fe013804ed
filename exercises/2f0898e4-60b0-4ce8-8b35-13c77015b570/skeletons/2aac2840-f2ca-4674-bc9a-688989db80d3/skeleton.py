9951adf537d34981b2dc77c2004c63af2b296586
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-08 13:15:58 +0100

    created file exercises/2f0898e4-60b0-4ce8-8b35-13c77015b570/skeletons/2aac2840-f2ca-4674-bc9a-688989db80d3/skeleton.py

---
+++ exercises/2f0898e4-60b0-4ce8-8b35-13c77015b570/skeletons/2aac2840-f2ca-4674-bc9a-688989db80d3/skeleton.py
@@ -0,0 +1,4 @@
+#In the program below, add a condition that checks if the value of the variable <i> age </i> is less than 18, in that case <i> child </i> should be displayed. As next in the first line, change the value of <i> age </i> to 8 and run the program. 
+wiek = 27
+if wiek >= 18:
+   print('adult')

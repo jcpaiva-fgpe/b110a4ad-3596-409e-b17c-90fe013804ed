2851cb0112e8166bb2c2d4b35f3cc7a6b7de35ea
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-09 18:31:34 +0100

    created file exercises/e61f0a7b-bda5-4522-ad84-2201cc0746c7/solutions/4caac099-0614-4be0-a1ac-6b61536cc2a0/sol.py

---
+++ exercises/e61f0a7b-bda5-4522-ad84-2201cc0746c7/solutions/4caac099-0614-4be0-a1ac-6b61536cc2a0/sol.py
@@ -0,0 +1,10 @@
+grade = 5.0
+
+if grade == 5.0:
+    print ('Congratulations!')
+elif grade >= 4.0:
+    print ('Not bad!')
+elif grade >= 3.0:
+    print ('Made it again!')
+else:
+    print ('What a pity')

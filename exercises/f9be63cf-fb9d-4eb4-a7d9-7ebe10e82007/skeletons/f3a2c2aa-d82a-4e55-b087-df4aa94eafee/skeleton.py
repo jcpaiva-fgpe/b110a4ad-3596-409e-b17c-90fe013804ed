0fbdafcdac835c3facf2ae808a8f9284e9b15582
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-09 15:47:06 +0100

    updated file exercises/f9be63cf-fb9d-4eb4-a7d9-7ebe10e82007/skeletons/f3a2c2aa-d82a-4e55-b087-df4aa94eafee/skeleton.py

--- exercises/f9be63cf-fb9d-4eb4-a7d9-7ebe10e82007/skeletons/f3a2c2aa-d82a-4e55-b087-df4aa94eafee/skeleton.py
+++ exercises/f9be63cf-fb9d-4eb4-a7d9-7ebe10e82007/skeletons/f3a2c2aa-d82a-4e55-b087-df4aa94eafee/skeleton.py
@@ -3,7 +3,7 @@ employment_contracts = input()
 print('Commuter?')
 commuter = input()
 
-# Add your code here
+# Insert your code here
 costs = 1335.00 
 
 print('The annual costs of revenue', costs, u'zł.')

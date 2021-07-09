aac6a281885df6933c08dcbaf1ad8117e4d72834
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-09 18:30:43 +0100

    created file exercises/e61f0a7b-bda5-4522-ad84-2201cc0746c7/skeletons/5ebe0bf3-520f-4778-84a7-d7473dd87e8c/skeleton.py

---
+++ exercises/e61f0a7b-bda5-4522-ad84-2201cc0746c7/skeletons/5ebe0bf3-520f-4778-84a7-d7473dd87e8c/skeleton.py
@@ -0,0 +1,9 @@
+grade = 5.0
+if grade == 5.0:
+    print ('Congratulations!')
+if grade >= 4.0:
+    print ('Not bad!')
+if grade >= 3.0:
+    print ('Made it again!')
+else:
+    print ('What a pity')

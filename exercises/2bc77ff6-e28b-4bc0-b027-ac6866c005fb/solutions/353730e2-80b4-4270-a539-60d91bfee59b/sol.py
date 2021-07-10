19dadd3439652346717846e955a36f1735289fb7
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-10 19:29:32 +0100

    updated file exercises/2bc77ff6-e28b-4bc0-b027-ac6866c005fb/solutions/353730e2-80b4-4270-a539-60d91bfee59b/sol.py

--- exercises/2bc77ff6-e28b-4bc0-b027-ac6866c005fb/solutions/353730e2-80b4-4270-a539-60d91bfee59b/sol.py
+++ exercises/2bc77ff6-e28b-4bc0-b027-ac6866c005fb/solutions/353730e2-80b4-4270-a539-60d91bfee59b/sol.py
@@ -2,5 +2,5 @@ age = int(input('How old are you?\n'))
 
 while age < 18:
     print(f'Still {18-age} years until the age of majority.')
-    age = input('How old are you now?\n')
+    age = int(input('How old are you now?\n'))
 print('Finally adult!')

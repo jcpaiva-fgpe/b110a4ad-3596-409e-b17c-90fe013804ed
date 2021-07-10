8be9768d80c9dd13577211c63d934ebbf4d6ec82
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-10 20:36:09 +0100

    updated file exercises/bf33c8c9-e3bb-441e-873e-81412cd26cff/solutions/de970b15-67cf-40f1-9f8d-f439414c4872/sol.py

--- exercises/bf33c8c9-e3bb-441e-873e-81412cd26cff/solutions/de970b15-67cf-40f1-9f8d-f439414c4872/sol.py
+++ exercises/bf33c8c9-e3bb-441e-873e-81412cd26cff/solutions/de970b15-67cf-40f1-9f8d-f439414c4872/sol.py
@@ -3,6 +3,6 @@ amount=0
 
 for divisor in range (dividend-1, 1, -1):
     if dividend % divisor == 0:
-        print(dividend, 'is divided without any remainder by', divisor)
+        print(dividend, 'is divided without a remainder by', divisor)
         amount += 1
-print('In total', dividend, 'is divided without any remainder', amount, 'times.')
+print('In total', dividend, 'is divided without a remainder', amount, 'times.')

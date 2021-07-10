81e30af05b12fb2bad3ac21e6504443c082176d7
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-10 07:52:52 +0100

    updated file exercises/2bc77ff6-e28b-4bc0-b027-ac6866c005fb/solutions/353730e2-80b4-4270-a539-60d91bfee59b/sol.py

--- exercises/2bc77ff6-e28b-4bc0-b027-ac6866c005fb/solutions/353730e2-80b4-4270-a539-60d91bfee59b/sol.py
+++ exercises/2bc77ff6-e28b-4bc0-b027-ac6866c005fb/solutions/353730e2-80b4-4270-a539-60d91bfee59b/sol.py
@@ -1,7 +1,9 @@
 age = int(input('How old are you?\n'))
 
 while age < 18:
-    print(f'You are still {18-age} under.')    
-    
+    print(f'You are still {18-age} until the age of majority.')
+        
     age = input('How old are you now?\n')
     print('Finally adult!')
+
+# Start the program, enter 7 as your age first and 21 as your second try.

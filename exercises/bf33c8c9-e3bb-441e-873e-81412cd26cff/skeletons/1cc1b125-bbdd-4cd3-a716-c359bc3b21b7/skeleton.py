0329bdf89c5ecf7ca1da018261fe2ce709109376
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-10 13:08:56 +0100

    updated file exercises/bf33c8c9-e3bb-441e-873e-81412cd26cff/skeletons/1cc1b125-bbdd-4cd3-a716-c359bc3b21b7/skeleton.py

--- exercises/bf33c8c9-e3bb-441e-873e-81412cd26cff/skeletons/1cc1b125-bbdd-4cd3-a716-c359bc3b21b7/skeleton.py
+++ exercises/bf33c8c9-e3bb-441e-873e-81412cd26cff/skeletons/1cc1b125-bbdd-4cd3-a716-c359bc3b21b7/skeleton.py
@@ -1,4 +1,4 @@
 dividend = int(input('dividend:')) 
 for divisor in range (dividend-1,1, -1):
     if dividend % divisor == 0:
-        print()
+        print(dividend, 'is divided without any remainder by', divisor)

17c6b1137654ad034896309505f40a7af765ae13
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-10 15:45:05 +0100

    updated file exercises/086820e0-fe0c-4c0f-b776-57b29b07068e/skeletons/332f54e8-5b33-451d-9a4c-1078f7e198ab/skeleton.py

--- exercises/086820e0-fe0c-4c0f-b776-57b29b07068e/skeletons/332f54e8-5b33-451d-9a4c-1078f7e198ab/skeleton.py
+++ exercises/086820e0-fe0c-4c0f-b776-57b29b07068e/skeletons/332f54e8-5b33-451d-9a4c-1078f7e198ab/skeleton.py
@@ -4,6 +4,7 @@ country = '?'
 while (True):
     print('Name the country:')
     country = input()
+    
     if not country: break 
     if not country:
     	print('The name was mentioned before!')

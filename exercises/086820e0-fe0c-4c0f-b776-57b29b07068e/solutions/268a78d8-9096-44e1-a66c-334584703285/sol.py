5d98209ec6322cb87a21a0412396560f2dfb9938
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-10 15:44:12 +0100

    created file exercises/086820e0-fe0c-4c0f-b776-57b29b07068e/solutions/268a78d8-9096-44e1-a66c-334584703285/sol.py

---
+++ exercises/086820e0-fe0c-4c0f-b776-57b29b07068e/solutions/268a78d8-9096-44e1-a66c-334584703285/sol.py
@@ -0,0 +1,9 @@
+countries = set()
+country = '?'
+
+while (True):
+    print('Name the country:')
+    country = input()
+    if not country: break 
+    if not country:
+    	print('The name was mentioned before!')

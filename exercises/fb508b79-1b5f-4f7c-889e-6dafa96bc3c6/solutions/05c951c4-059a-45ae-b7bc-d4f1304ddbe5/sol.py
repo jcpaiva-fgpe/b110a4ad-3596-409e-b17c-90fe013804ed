63907f1f5227288f9e2124e314201249d84da622
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-14 12:37:41 +0100

    updated file exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/solutions/05c951c4-059a-45ae-b7bc-d4f1304ddbe5/sol.py

--- exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/solutions/05c951c4-059a-45ae-b7bc-d4f1304ddbe5/sol.py
+++ exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/solutions/05c951c4-059a-45ae-b7bc-d4f1304ddbe5/sol.py
@@ -1,6 +1,8 @@
+w = {'energy emergency': 'pogotowie energetyczne', 'gas emergency': 'pogotowie gazowe', 'police': 'policja', 'fire brigade': u'straż pożarna', 'emergency': 'pogotowie ratunkowe'}
+
 while True:
     word = input()
     if not word: break
-    elif(word not in w.keys()): msg = "Unknown word"
+    elif(word not in w.keys()): msg = "Unknown key"
     else: msg = w[word]
     print(msg)

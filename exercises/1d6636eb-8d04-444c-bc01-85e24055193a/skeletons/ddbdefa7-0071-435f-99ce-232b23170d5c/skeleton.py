a65d48d12c1e0857d3ea3bfbdda6c161dc8a2efd
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-17 09:11:37 +0100

    updated file exercises/1d6636eb-8d04-444c-bc01-85e24055193a/skeletons/ddbdefa7-0071-435f-99ce-232b23170d5c/skeleton.py

--- exercises/1d6636eb-8d04-444c-bc01-85e24055193a/skeletons/ddbdefa7-0071-435f-99ce-232b23170d5c/skeleton.py
+++ exercises/1d6636eb-8d04-444c-bc01-85e24055193a/skeletons/ddbdefa7-0071-435f-99ce-232b23170d5c/skeleton.py
@@ -1,6 +1,17 @@
+def power(x, n):
+    return x**n
+    
+print(3, '^', 4, 'is:', power(3, 4))
+print(4, '^', 3, 'is:', power(4, 3))
+print(3, '^', 4, 'is:', power(x=3, n=4))
+print(4, '^', 3, 'is:', power(n=3, x=4))
+
+
+# Your root function
 def root(x):
 	return x**0.5
 
 x = int(input())
 
 print(str(2) + '-nth root of', x, 'is:', root(x))
+#print(y + '-nth root of', x, 'is:', root(x,y))

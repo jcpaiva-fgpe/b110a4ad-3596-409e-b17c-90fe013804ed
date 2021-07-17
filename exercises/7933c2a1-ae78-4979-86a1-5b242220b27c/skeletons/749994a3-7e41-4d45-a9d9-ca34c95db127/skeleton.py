4c1c2b36617bc1cc2b184312b531fc664ee5a529
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-17 12:31:31 +0100

    created file exercises/7933c2a1-ae78-4979-86a1-5b242220b27c/skeletons/749994a3-7e41-4d45-a9d9-ca34c95db127/skeleton.py

---
+++ exercises/7933c2a1-ae78-4979-86a1-5b242220b27c/skeletons/749994a3-7e41-4d45-a9d9-ca34c95db127/skeleton.py
@@ -0,0 +1,26 @@
+def avg():
+
+
+    return "Call the function with at least one arg."
+
+
+
+
+### Do not change that code here ##### 
+print(avg(*map(int, input().split())))  
+######################################
+
+""" <-- Example: PUT A # ON THE BEGINNING of the line.
+
+x, y = map(int, input().split())
+
+x = input()
+y = input()
+
+print(avg())         # Withouut arg 
+print(avg(x))        # one arg
+print(avg(x,y))      # two args
+
+#""" 
+####### EOT - End of test. ##########
+#####################################

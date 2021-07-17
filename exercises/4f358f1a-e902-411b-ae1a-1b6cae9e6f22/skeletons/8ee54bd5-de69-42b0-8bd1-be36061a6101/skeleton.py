5e9b22173007dd16d64c1252f9be0fac0b50cf84
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-17 11:40:42 +0100

    updated file exercises/4f358f1a-e902-411b-ae1a-1b6cae9e6f22/skeletons/8ee54bd5-de69-42b0-8bd1-be36061a6101/skeleton.py

--- exercises/4f358f1a-e902-411b-ae1a-1b6cae9e6f22/skeletons/8ee54bd5-de69-42b0-8bd1-be36061a6101/skeleton.py
+++ exercises/4f358f1a-e902-411b-ae1a-1b6cae9e6f22/skeletons/8ee54bd5-de69-42b0-8bd1-be36061a6101/skeleton.py
@@ -1,10 +1,22 @@
-def a_n():
-    pass
+def div(a=2, b=3):
+    return a/b
+
+"""
+print(div())
+print(div(1))
+print(div(1, 2))
+print(div(2, 1))
+#"""
 
 
+def stars():
+    
+	pass
 
+
+# ___________________________________
+#  Test for stars CAN'T TOUCH THIS   #
 # ---------------------------------- #
-# ! Test data ### CAN'T TOUCH THIS ! #
-# ---------------------------------- #
-print(a_n(*map(int, input().split())))
-# ---------------------------------- #
+try: print(stars(int(input())))      #
+except: print(stars())               #
+# __________________________________ /

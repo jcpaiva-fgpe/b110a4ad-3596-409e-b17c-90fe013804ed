749165cfc7430961d9c57f367ae1c81d34c10cc0
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-19 23:02:42 +0100

    updated file exercises/86de2b84-ffd2-4dd3-9ee5-d98322989f90/solutions/7f939221-7c87-452c-9b04-a62fea268997/sol.py

--- exercises/86de2b84-ffd2-4dd3-9ee5-d98322989f90/solutions/7f939221-7c87-452c-9b04-a62fea268997/sol.py
+++ exercises/86de2b84-ffd2-4dd3-9ee5-d98322989f90/solutions/7f939221-7c87-452c-9b04-a62fea268997/sol.py
@@ -18,6 +18,7 @@ def rk(a, b, c):
 try:
     print(rk(*map(int, input().split())))  
 except:
+    #print(smash(1.2) #Example function #
     print(rk(-1,2,3))                  #
     print(rk(1,2,1))                   #
     print(rk(2,1,1))                   #

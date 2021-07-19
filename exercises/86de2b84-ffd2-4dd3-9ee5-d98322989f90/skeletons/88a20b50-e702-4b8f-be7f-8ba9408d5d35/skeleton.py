5057214cda29ae765e8f60cac429dd0b2d61bf5f
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-19 22:38:25 +0100

    created file exercises/86de2b84-ffd2-4dd3-9ee5-d98322989f90/skeletons/88a20b50-e702-4b8f-be7f-8ba9408d5d35/skeleton.py

---
+++ exercises/86de2b84-ffd2-4dd3-9ee5-d98322989f90/skeletons/88a20b50-e702-4b8f-be7f-8ba9408d5d35/skeleton.py
@@ -0,0 +1,23 @@
+# Example
+def smash(x):
+    return int(x), x%1
+
+print(smash(1.2))
+
+#Your function
+
+
+
+
+
+
+# ------------------------------------ #
+#     Do not change that code here     | 
+# ------------------------------------ #
+try:
+    print(rk(*map(int, input().split())))  
+except:
+    print(rk(-1,2,3))                  #
+    print(rk(1,2,1))                   #
+    print(rk(2,1,1))                   #
+# ------------------------------------ #

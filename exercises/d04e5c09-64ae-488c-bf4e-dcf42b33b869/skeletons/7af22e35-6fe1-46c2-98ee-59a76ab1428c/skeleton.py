0f0bba8f69ba73528e05741c691121dbbc76769b
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-19 23:42:46 +0100

    updated file exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/skeletons/7af22e35-6fe1-46c2-98ee-59a76ab1428c/skeleton.py

--- exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/skeletons/7af22e35-6fe1-46c2-98ee-59a76ab1428c/skeleton.py
+++ exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/skeletons/7af22e35-6fe1-46c2-98ee-59a76ab1428c/skeleton.py
@@ -8,35 +8,16 @@ def old_date(d):
 
 
 
-
 #Your function
 def bli():
-
+    
     pass
+#print(bli([3,6,8,1,4], 3))
 
-
-"""
-print(bli([3,6,8,1,4], 3))
-# """
 
 #############################################
 # TEST DATA ### DON'T TOUCH THIS.           #
 # ----------------------------------------- #
-#print(bli(input().split(), int(input())))   #
+print(bli(input().split(), int(input())))   #
 #############################################
-
-
-""" <-- PUT A # ON THE BEGINNING of the line.
-print(bili([3, 6, 8, 1, 4], 3))
-#print(bili([2, 1, 8, 9, 4, 5], 2))
-#print(bili([5], 1))
-#""" 
-
-try:
-    li = list(map(int, input().split(','))) # li - list, seqence of numbers (sep. by ',').
-    b = int(input())                        # b - number.
-    print(bli(li, b))                       # Call the function and display the result.
-except ValueError:
-    print("Wrong input data.")              #
-# ----------------------------------------- #
 

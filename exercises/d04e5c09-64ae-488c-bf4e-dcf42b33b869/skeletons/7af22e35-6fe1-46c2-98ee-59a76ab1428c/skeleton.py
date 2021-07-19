76263496e659921ec4a1da1960f1f5326582a981
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-19 23:39:51 +0100

    updated file exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/skeletons/7af22e35-6fe1-46c2-98ee-59a76ab1428c/skeleton.py

--- exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/skeletons/7af22e35-6fe1-46c2-98ee-59a76ab1428c/skeleton.py
+++ exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/skeletons/7af22e35-6fe1-46c2-98ee-59a76ab1428c/skeleton.py
@@ -1,3 +1,14 @@
+#EXAMPLE FUNCTION old_date()
+def old_date(d):
+    def month():
+        return ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII'][int(d[1])-1]
+    d = d.split('-')
+    return d[2]+'.'+month()+'.'+d[0]
+#print(old_date('2019-11-24'))
+
+
+
+
 #Your function
 def bli():
 

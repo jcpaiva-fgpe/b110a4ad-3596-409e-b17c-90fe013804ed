318e77fd819ff065b0da38f695de4ef72df5d9e2
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-19 23:57:30 +0100

    created file exercises/f012893b-af6c-403b-a643-c8dfda129284/skeletons/e50ef2a0-6099-47f8-99d7-b1fe48c56ba9/skeleton.py

---
+++ exercises/f012893b-af6c-403b-a643-c8dfda129284/skeletons/e50ef2a0-6099-47f8-99d7-b1fe48c56ba9/skeleton.py
@@ -0,0 +1,8 @@
+def register(entry):
+    global log
+    log += [entry] 
+    log = []
+    register("Nice nice weather. I'm fine.")
+    register("It's bad weather. I feel bad.")
+    register ("The weather is so-so. I feel average.")
+print ('\n'.join(log)) 

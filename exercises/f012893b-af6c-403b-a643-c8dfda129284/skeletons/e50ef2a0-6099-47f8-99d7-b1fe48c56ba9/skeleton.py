10d1e95f1cc1fd222e84379bf12b47a8138eb1e3
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-20 00:07:56 +0100

    updated file exercises/f012893b-af6c-403b-a643-c8dfda129284/skeletons/e50ef2a0-6099-47f8-99d7-b1fe48c56ba9/skeleton.py

--- exercises/f012893b-af6c-403b-a643-c8dfda129284/skeletons/e50ef2a0-6099-47f8-99d7-b1fe48c56ba9/skeleton.py
+++ exercises/f012893b-af6c-403b-a643-c8dfda129284/skeletons/e50ef2a0-6099-47f8-99d7-b1fe48c56ba9/skeleton.py
@@ -1,3 +1,4 @@
+#EXAMPLE FUNCTION register()
 def register(entry):
     global log
     log += [entry]
@@ -7,3 +8,6 @@ register("Nice nice weather. I'm fine.")
 register("It's bad weather. I feel bad.")
 register ("The weather is so-so. I feel average.")
 print('\n'.join(log))
+
+
+

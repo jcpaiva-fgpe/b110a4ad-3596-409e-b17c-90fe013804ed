c8061a476d94a1a9331b9faf82193e5e4f663691
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-19 23:44:10 +0100

    updated file exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/solutions/ae17efaf-a665-4d6b-a14b-4211652ff269/sol.py

--- exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/solutions/ae17efaf-a665-4d6b-a14b-4211652ff269/sol.py
+++ exercises/d04e5c09-64ae-488c-bf4e-dcf42b33b869/solutions/ae17efaf-a665-4d6b-a14b-4211652ff269/sol.py
@@ -9,15 +9,19 @@ def bli(li, b):
     li.remove(bli1())
     return(bli(li, b-1))
 
+#############################################
+
+
+""" <-- PUT A # ON THE BEGINNING of the line.
+print(bli([3, 6, 8, 1, 4], 3))
+#print(bli([2, 1, 8, 9, 4, 5], 2))
+#print(bli([5], 1))
+#""" 
+
 try:
-    li = list(map(int, input().split(','))) # Expecting a seqence of numbers (sep. by ',')
-    b = int(input())
-    print(bli(li, b)) # Call the function and display the result
-
+    li = list(map(int, input().split()))    # li - list, seqence of numbers (sep. by ',').
+    b = int(input())                        # b - number.
+    print(bli(li, b))                       # Call the function and display the result.
 except ValueError:
-    print("Wrong input data.")
-
-
-"""
-print(bli([3,6,8,1,4], 3))
-# """
+    print("Wrong input data.")              #
+# ----------------------------------------- #

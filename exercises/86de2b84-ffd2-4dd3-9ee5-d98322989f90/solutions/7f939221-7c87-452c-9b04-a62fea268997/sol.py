b3da14e32159074ff42c985926eec757ba781b54
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-19 22:39:35 +0100

    created file exercises/86de2b84-ffd2-4dd3-9ee5-d98322989f90/solutions/7f939221-7c87-452c-9b04-a62fea268997/sol.py

---
+++ exercises/86de2b84-ffd2-4dd3-9ee5-d98322989f90/solutions/7f939221-7c87-452c-9b04-a62fea268997/sol.py
@@ -0,0 +1,24 @@
+
+def rk(a, b, c):
+
+    delta = b**2 - 4*a*c
+    if delta == 0:
+        return -b/2*a
+
+    elif delta > 0:
+        x1 = (-b + delta**0.5)/2*a
+        x2 = (-b - delta**0.5)/2*a
+        return x1, x2
+    else: return
+
+
+# ------------------------------------ #
+#     Do not change that code here     | 
+# ------------------------------------ #
+try:
+    print(rk(*map(int, input().split())))  
+except:
+    print(rk(-1,2,3))                  #
+    print(rk(1,2,1))                   #
+    print(rk(2,1,1))                   #
+# ------------------------------------ #

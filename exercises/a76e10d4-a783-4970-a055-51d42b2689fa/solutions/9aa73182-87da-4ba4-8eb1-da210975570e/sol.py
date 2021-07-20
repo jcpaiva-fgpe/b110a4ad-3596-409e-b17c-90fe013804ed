117b1b2a2d12d673a1e23de4bda4bd047c9f4d56
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-20 12:51:32 +0100

    updated file exercises/a76e10d4-a783-4970-a055-51d42b2689fa/solutions/9aa73182-87da-4ba4-8eb1-da210975570e/sol.py

--- exercises/a76e10d4-a783-4970-a055-51d42b2689fa/solutions/9aa73182-87da-4ba4-8eb1-da210975570e/sol.py
+++ exercises/a76e10d4-a783-4970-a055-51d42b2689fa/solutions/9aa73182-87da-4ba4-8eb1-da210975570e/sol.py
@@ -1,7 +1,12 @@
-while True:
-    i = input()
-    if not i: 
-        break
-    else:
-        print("OK")
-        break
+collection = set(('Thorgal. Part I', 'Thorgal. Part II', 'Kayko and Kokosh. Part I',  'Kayko and Kokosh. Part II' ))
+thorgal = set(('Thorgal. Part I', 'Thorgal. Part II', 'Thorgal. Part III' ))
+kayko = set(('Kayko and Kokosh. Part I',  'Kayko and Kokosh. Part II' ))
+print('You are a lucky owner of following Kayko and Kokosh series:')
+for t in kayko.intersection(collection):
+    print(t)
+if thorgal <= collection.intersection(thorgal):
+    print('You have the complete Thorgal series.')
+else:
+    print ('You are missing the following Thorgal series:')
+    for t in thorgal.symmetric_difference(collection.intersection(thorgal)):
+        print(t)

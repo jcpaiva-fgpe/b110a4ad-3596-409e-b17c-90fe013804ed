5ba96a56914e90a4ef7c92e8794b5d2c5e584164
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-20 07:59:43 +0100

    updated file exercises/4f358f1a-e902-411b-ae1a-1b6cae9e6f22/solutions/ee8b9306-603d-4567-ae4c-81fcd20ce0a1/sol.py

--- exercises/4f358f1a-e902-411b-ae1a-1b6cae9e6f22/solutions/ee8b9306-603d-4567-ae4c-81fcd20ce0a1/sol.py
+++ exercises/4f358f1a-e902-411b-ae1a-1b6cae9e6f22/solutions/ee8b9306-603d-4567-ae4c-81fcd20ce0a1/sol.py
@@ -1,2 +1,10 @@
 def stars(num=15):
 	return '*'*num
+
+
+# ---------------------------------- #
+# Test stars(). CAN'T TOUCH THIS.    #
+# ---------------------------------- #
+try: print(stars(int(input())))      #
+except: print(stars())               #
+# ---------------------------------- #

1b1e8ea747e3eed7492b54c89ef0a42b612ef860
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-21 17:58:06 +0100

    created file exercises/60804816-7e5a-4574-9d99-666b611daa7b/solutions/71a13b04-7692-468c-9831-4fba1994ac5f/sol.py

---
+++ exercises/60804816-7e5a-4574-9d99-666b611daa7b/solutions/71a13b04-7692-468c-9831-4fba1994ac5f/sol.py
@@ -0,0 +1,26 @@
+class Clothes:
+    def __init__(u, material, color):
+        u.material = material
+        u.color = color
+    def description(u):
+        return u.material + ' in color: ' + u.color
+class Jacket(Clothes):
+     def leather(u):
+         return u.material == 'leather'
+class Trousers(Clothes):
+     def summer(u):
+         return u.color == 'white'
+class Jeans(Trousers):
+     def __init__(object):
+         object.color = 'blue'
+         object.material = 'jeans'
+
+u = Clothes('wool', 'gray')
+k = Jacket('leather ','black')
+s = Trousers('jeans', 'blue')
+j = Jeans()
+
+print(u.description())
+print(k.leather())
+print(s.summer())
+print(j.description())

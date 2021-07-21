101e40ca13f8f1d4e736aa469a59f94516e9e94a
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-21 18:11:39 +0100

    updated file exercises/60804816-7e5a-4574-9d99-666b611daa7b/solutions/71a13b04-7692-468c-9831-4fba1994ac5f/sol.py

--- exercises/60804816-7e5a-4574-9d99-666b611daa7b/solutions/71a13b04-7692-468c-9831-4fba1994ac5f/sol.py
+++ exercises/60804816-7e5a-4574-9d99-666b611daa7b/solutions/71a13b04-7692-468c-9831-4fba1994ac5f/sol.py
@@ -1,26 +1,11 @@
-class Clothes:
-    def __init__(u, material, color):
-        u.material = material
-        u.color = color
-    def description(u):
-        return u.material + ' in color: ' + u.color
-class Jacket(Clothes):
-     def leather(u):
-         return u.material == 'leather'
-class Trousers(Clothes):
-     def summer(u):
-         return u.color == 'white'
-class Jeans(Trousers):
-     def __init__(object):
-         object.color = 'blue'
-         object.material = 'jeans'
-
-u = Clothes('wool', 'gray')
-k = Jacket('leather ','black')
-s = Trousers('jeans', 'blue')
-j = Jeans()
-
-print(u.description())
-print(k.leather())
-print(s.summer())
-print(j.description())
+class Rectangle:
+    a = b = 1.0
+    def __init___(f, a, b):
+        f.a = a
+        f.b = b
+    def __str__(f) -> str:
+        return f'{f.a}{f.b}'
+    def perimeter(f):
+        return 2*f.a + 2*f.b
+    def area(f):
+        return f.a*f.b

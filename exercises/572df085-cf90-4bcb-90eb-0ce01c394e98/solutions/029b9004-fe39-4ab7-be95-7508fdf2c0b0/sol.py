7ae56b203c5f84cad6b44f7f8647ce8c2867bb05
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-21 11:29:10 +0100

    updated file exercises/572df085-cf90-4bcb-90eb-0ce01c394e98/solutions/029b9004-fe39-4ab7-be95-7508fdf2c0b0/sol.py

--- exercises/572df085-cf90-4bcb-90eb-0ce01c394e98/solutions/029b9004-fe39-4ab7-be95-7508fdf2c0b0/sol.py
+++ exercises/572df085-cf90-4bcb-90eb-0ce01c394e98/solutions/029b9004-fe39-4ab7-be95-7508fdf2c0b0/sol.py
@@ -1,7 +1,9 @@
-class Square: 
-   a = 1.0
-   def perimeter(f):
-       return f.a * 4.0
+class Square:
+    a = 1.0
+    def perimeter(f):
+        return f.a * 4.0
+    def __str__(f):
+        return f'Side of the square is a = {f.a}'
        
 k = Square()
 print(k)

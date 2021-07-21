dbbc6e01221d7c4343c8d85a853bbfe7ffa7ceba
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-21 18:00:51 +0100

    updated file exercises/60804816-7e5a-4574-9d99-666b611daa7b/skeletons/30368991-7815-4030-a766-b1fe89ce88fe/skeleton.py

--- exercises/60804816-7e5a-4574-9d99-666b611daa7b/skeletons/30368991-7815-4030-a766-b1fe89ce88fe/skeleton.py
+++ exercises/60804816-7e5a-4574-9d99-666b611daa7b/skeletons/30368991-7815-4030-a766-b1fe89ce88fe/skeleton.py
@@ -5,22 +5,16 @@ class Clothes:
     def description(u):
         return u.material + ' in color: ' + u.color
 class Jacket(Clothes):
-     def leather(u):
-         return u.material == 'leather'
+    def leather(u):
+        return u.material == 'leather'
 class Trousers(Clothes):
-     def summer(u):
-         return u.color == 'white'
-class Jeans(Trousers):
-     def __init__(object):
-         object.color = 'blue'
-         object.material = 'jeans'
+    def summer(u):
+        return u.color == 'white'
 
 u = Clothes('wool', 'gray')
-k = Jacket('leather ','black')
+k = Jacket('leather ', 'black')
 s = Trousers('jeans', 'blue')
-j = Jeans()
 
 print(u.description())
 print(k.leather())
 print(s.summer())
-print(j.description())

b3a2ac3acee9dc48c2e72d90ba1805c43a01be4b
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-22 08:36:06 +0100

    updated file exercises/8aa0d693-655e-4013-aa89-7e6cc00f4fcd/solutions/121322f2-3478-4b3c-9374-d1d3e99772a6/sol.py

--- exercises/8aa0d693-655e-4013-aa89-7e6cc00f4fcd/solutions/121322f2-3478-4b3c-9374-d1d3e99772a6/sol.py
+++ exercises/8aa0d693-655e-4013-aa89-7e6cc00f4fcd/solutions/121322f2-3478-4b3c-9374-d1d3e99772a6/sol.py
@@ -5,18 +5,18 @@ class Clothes:
     def description(u):
         return u.material + ' in color: ' + u.color
 class Jacket(Clothes):
-     def leather(u):
-         return u.material == 'leather'
+    def leather(u):
+        return u.material == 'leather'
 class Trousers(Clothes):
-     def summer(u):
-         return u.color == 'white'
+    def summer(u):
+        return u.color == 'white'
 class Jeans(Trousers):
-     def __init__(object):
-         object.color = 'blue'
-         object.material = 'jeans'
+    def __init__(object):
+        object.color = 'blue'
+        object.material = 'jeans'
 
 u = Clothes('wool', 'gray')
-k = Jacket('leather ','black')
+k = Jacket('leather', 'black')
 s = Trousers('jeans', 'blue')
 j = Jeans()
 

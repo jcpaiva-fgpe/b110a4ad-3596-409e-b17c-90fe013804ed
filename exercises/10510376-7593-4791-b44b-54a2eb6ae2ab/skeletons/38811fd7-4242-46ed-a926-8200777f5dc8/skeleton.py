88858b920199546ba9082e6d77923454bc85ebe8
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-24 14:11:41 +0100

    created file exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py

---
+++ exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py
@@ -0,0 +1,7 @@
+def search_bin(list, searched):
+    #Your code here
+    pass
+
+x = input("Enter the value: ")
+r = search_bin(test, int(x))
+print(f"Found in position {r}.") if r >= 0 else print("Not found.")

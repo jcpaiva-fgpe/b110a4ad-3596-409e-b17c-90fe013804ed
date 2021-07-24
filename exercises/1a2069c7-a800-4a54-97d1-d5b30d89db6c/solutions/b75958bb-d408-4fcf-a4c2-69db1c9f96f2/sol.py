e1d5a01bacabb290de61ec1c5bf145ed1ea37caf
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-24 08:51:29 +0100

    created file exercises/1a2069c7-a800-4a54-97d1-d5b30d89db6c/solutions/b75958bb-d408-4fcf-a4c2-69db1c9f96f2/sol.py

---
+++ exercises/1a2069c7-a800-4a54-97d1-d5b30d89db6c/solutions/b75958bb-d408-4fcf-a4c2-69db1c9f96f2/sol.py
@@ -0,0 +1,13 @@
+def qsort(list_a):
+    if len(list_a) <= 1:
+        return list_a
+    else:
+        middle = list_a[len(list_a) // 2]
+        smaller = qsort([x for x in list_a if x < middle])
+        higher = qsort([x for x in list_a if x > middle])
+        the_same = [x for x in list_a if x == middle]
+        return smaller + the_same + higher
+
+test = input()
+print("List:", test)
+print("Sorted:", qsort(test))

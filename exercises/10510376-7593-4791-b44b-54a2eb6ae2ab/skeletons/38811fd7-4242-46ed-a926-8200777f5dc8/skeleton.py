0091c7efd9f1d06a80f9607dbd8d971a0e3d6c53
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-24 14:55:58 +0100

    updated file exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py

--- exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py
+++ exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py
@@ -3,10 +3,11 @@ def search_bin(list, searched):
     pass
 
 
-### Can't touch this. ###
-# ==================================== #
+# ===================================== #
+###         Can't touch this.         ###
+# ===================================== #
 test = list(map(int, input().split()))
 x = input("Val:\n")
 r = search_bin(test, int(x))
 print(f"Found in position {r}.") if r >= 0 else print("Not found.")
-# ==================================== #
+# ===================================== #

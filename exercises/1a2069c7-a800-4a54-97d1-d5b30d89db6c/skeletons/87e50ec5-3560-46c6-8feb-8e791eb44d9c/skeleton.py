d182cb72fd68f301a006e6da8f2ac58c56dcce66
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-24 13:34:40 +0100

    updated file exercises/1a2069c7-a800-4a54-97d1-d5b30d89db6c/skeletons/87e50ec5-3560-46c6-8feb-8e791eb44d9c/skeleton.py

--- exercises/1a2069c7-a800-4a54-97d1-d5b30d89db6c/skeletons/87e50ec5-3560-46c6-8feb-8e791eb44d9c/skeleton.py
+++ exercises/1a2069c7-a800-4a54-97d1-d5b30d89db6c/skeletons/87e50ec5-3560-46c6-8feb-8e791eb44d9c/skeleton.py
@@ -2,6 +2,10 @@ def qsort(li):
     #Your code here
     pass
 
+
+### Can't touch this. ###
 test = list(map(int, input().split()))
+# ===================== #
 print("List:", test)
 print("Sorted:", qsort(test))
+# ===================== #

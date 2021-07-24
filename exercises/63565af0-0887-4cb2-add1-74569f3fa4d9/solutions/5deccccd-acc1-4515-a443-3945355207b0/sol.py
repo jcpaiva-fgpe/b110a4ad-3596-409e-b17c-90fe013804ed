0da3d6f68bc03792c3d1bec546ec01a080fd20d3
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-24 09:02:31 +0100

    created file exercises/63565af0-0887-4cb2-add1-74569f3fa4d9/solutions/5deccccd-acc1-4515-a443-3945355207b0/sol.py

---
+++ exercises/63565af0-0887-4cb2-add1-74569f3fa4d9/solutions/5deccccd-acc1-4515-a443-3945355207b0/sol.py
@@ -0,0 +1,10 @@
+class Memo():
+    calculated = {0: 0, 1: 1}
+    @classmethod
+    def fib(cls, n):
+        if n not in cls.calculated:
+            cls.calculated[n] = cls.fib(n - 1) + cls.fib(n - 2)
+        return cls.calculated[n]
+
+n = int(input("n:\n"))
+print("Fib(n) = ", Memo.fib(n))

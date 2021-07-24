3b87b69dd6de5476be8f18de889be198382efea4
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-24 14:53:16 +0100

    updated file exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py

--- exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py
+++ exercises/10510376-7593-4791-b44b-54a2eb6ae2ab/skeletons/38811fd7-4242-46ed-a926-8200777f5dc8/skeleton.py
@@ -7,7 +7,6 @@ def search_bin(list, searched):
 # ==================================== #
 test = list(map(int, input().split()))
 x = input("Val:\n")
-# ==================================== #
 r = search_bin(test, int(x))
 print(f"Found in position {r}.") if r >= 0 else print("Not found.")
 # ==================================== #

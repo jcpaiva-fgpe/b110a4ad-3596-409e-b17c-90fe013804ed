f90a2c398b223b25af1befd5361f71006758f374
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 21:52:11 +0100

    updated file exercises/3f540e3d-5602-437b-acde-731c805d8a8a/solutions/3000cd53-747c-4f45-9855-e61c4f229953/sol.py

--- exercises/3f540e3d-5602-437b-acde-731c805d8a8a/solutions/3000cd53-747c-4f45-9855-e61c4f229953/sol.py
+++ exercises/3f540e3d-5602-437b-acde-731c805d8a8a/solutions/3000cd53-747c-4f45-9855-e61c4f229953/sol.py
@@ -1,4 +1,4 @@
 tel = {'police': 997, 'fire brigade': 998, 'emergency': 999}
 inp = input()
-if not inp in tel.keys(): print("Unlisted number")
+if not inp in tel.keys(): print("False!")
 else: print(tel[inp])

dcb9faeaea63656e64fb1969afc62edd1d63459b
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 15:50:35 +0100

    updated file exercises/8aa0d693-655e-4013-aa89-7e6cc00f4fcd/skeletons/2e83d8c4-20fd-486d-8dc9-549a8ea5fd97/skeleton.py

--- exercises/8aa0d693-655e-4013-aa89-7e6cc00f4fcd/skeletons/2e83d8c4-20fd-486d-8dc9-549a8ea5fd97/skeleton.py
+++ exercises/8aa0d693-655e-4013-aa89-7e6cc00f4fcd/skeletons/2e83d8c4-20fd-486d-8dc9-549a8ea5fd97/skeleton.py
@@ -5,32 +5,16 @@ class Clothes:
     def description(u):
         return u.material + ' in color: ' + u.color
 class Jacket(Clothes):
-    def description(u):
-        return 'Jacket (' + Clothes.description(u) + ')'
-
-"""
+    def leather(u):
+        return u.material == 'leather'
 class Trousers(Clothes):
     def summer(u):
         return u.color == 'white'
-"""
-#u = Clothes('wool', 'gray')
-k = Jacket('leather ', 'black') # ang. skora, czarny
-#s = Trousers('jeans', 'blue')
 
-print(k.description())
-#print(k.leather())
-#print(s.summer())
+u = Clothes('wool', 'gray')
+k = Jacket('leather ', 'black')
+s = Trousers('jeans', 'blue')
 
-
-
-class Ubranie:\n\
-    def __init__(u,material,kolor):\n\
-        u.material = material\n\
-        u.kolor = kolor\n\
-    def opis(u):\n\
-        return u.material+' w kolorze: '+u.kolor\n\
-class Kurtka(Ubranie):\n\
-    def opis(u):\n\
-        return 'Kurtka ('+Ubranie.opis(u)+')'\n\
-k=Kurtka(u'skóra','czarny')\n\
-print k.opis()
+print(u.description())
+print(k.leather())
+print(s.summer())

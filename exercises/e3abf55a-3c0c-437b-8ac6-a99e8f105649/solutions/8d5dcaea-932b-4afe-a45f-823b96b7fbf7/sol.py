6f0c163f018cb3c590a0ea966dd5c9a2ebf3f78e
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 19:56:36 +0100

    updated file exercises/e3abf55a-3c0c-437b-8ac6-a99e8f105649/solutions/8d5dcaea-932b-4afe-a45f-823b96b7fbf7/sol.py

--- exercises/e3abf55a-3c0c-437b-8ac6-a99e8f105649/solutions/8d5dcaea-932b-4afe-a45f-823b96b7fbf7/sol.py
+++ exercises/e3abf55a-3c0c-437b-8ac6-a99e8f105649/solutions/8d5dcaea-932b-4afe-a45f-823b96b7fbf7/sol.py
@@ -1,7 +1,7 @@
-print('Write the 1st text:')
+print('String 1?')
 txt1 = input()
-print('Write the 2nd text:')
+print('String 2?')
 txt2 = input()
 
 common = set(txt1).intersection(set(txt2))
-print('Common letters are:', common)
+print('Common characters:', common)

2dbce108fb92499c4f0f3a7ae0409ec18835cead
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 22:41:30 +0100

    updated file exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/solutions/05c951c4-059a-45ae-b7bc-d4f1304ddbe5/sol.py

--- exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/solutions/05c951c4-059a-45ae-b7bc-d4f1304ddbe5/sol.py
+++ exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/solutions/05c951c4-059a-45ae-b7bc-d4f1304ddbe5/sol.py
@@ -1,9 +1,9 @@
 w = {'energy emergency': 'pogotowie energetyczne', 'gas emergency': 'pogotowie gazowe', 'police': 'policja', 'fire brigade': u'straż pożarna', 'emergency': 'pogotowie ratunkowe'}
 
 while True:
-    print("Enter a word to translate:")
+    print("Word?")
     word = input()
     if not word: break
-    elif(word not in w.keys()): msg = "Unknown word!"
+    elif(word not in w.keys()): msg = "False!"
     else: msg = w[word]
     print(msg)

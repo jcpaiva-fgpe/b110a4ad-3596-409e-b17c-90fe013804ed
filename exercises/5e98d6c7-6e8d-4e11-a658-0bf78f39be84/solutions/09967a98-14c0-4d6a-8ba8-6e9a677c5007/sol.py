f22040b388377c543997568e1251f28c79b5588e
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 22:28:54 +0100

    updated file exercises/5e98d6c7-6e8d-4e11-a658-0bf78f39be84/solutions/09967a98-14c0-4d6a-8ba8-6e9a677c5007/sol.py

--- exercises/5e98d6c7-6e8d-4e11-a658-0bf78f39be84/solutions/09967a98-14c0-4d6a-8ba8-6e9a677c5007/sol.py
+++ exercises/5e98d6c7-6e8d-4e11-a658-0bf78f39be84/solutions/09967a98-14c0-4d6a-8ba8-6e9a677c5007/sol.py
@@ -1,5 +1,6 @@
 tel = {'gas emergency': 992, 'police': 997, 'fire brigade': 998, 'emergency': 999}
 
 for k, v in tel.copy().items(): 
-    if v % 3 == 0: del(tel[k])
-print(tel)
+    if v % 3 == 0: 
+       print(k)
+       tel.pop(k)

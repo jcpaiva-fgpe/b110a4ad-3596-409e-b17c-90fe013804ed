9ac6c1aea7387e0833b18399bcf368d994ce82a1
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 22:42:36 +0100

    updated file exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/skeletons/f0aaa6bb-aad4-4758-a1f2-6d380a97416a/skeleton.py

--- exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/skeletons/f0aaa6bb-aad4-4758-a1f2-6d380a97416a/skeleton.py
+++ exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/skeletons/f0aaa6bb-aad4-4758-a1f2-6d380a97416a/skeleton.py
@@ -1,6 +1,6 @@
 w = {'energy emergency': 'pogotowie energetyczne', 'gas emergency': 'pogotowie gazowe', 'police': 'policja', 'fire brigade': u'straż pożarna', 'emergency': 'pogotowie ratunkowe'}
 
-print("Enter a word to translate:")
+print("Word?")
 word = input()
 
-print("Unknown word!")
+print("False!")

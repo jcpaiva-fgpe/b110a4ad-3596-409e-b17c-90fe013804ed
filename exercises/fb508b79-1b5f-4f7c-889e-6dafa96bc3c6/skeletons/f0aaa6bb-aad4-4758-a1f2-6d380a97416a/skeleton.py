ddcfd47141d1550adfb4b6ffaaed0eeb21b15ae9
Thomas Naprawski <tnaprawski@gmail.com>
2021-10-23 13:11:03 +0100

    updated file exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/skeletons/f0aaa6bb-aad4-4758-a1f2-6d380a97416a/skeleton.py

--- exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/skeletons/f0aaa6bb-aad4-4758-a1f2-6d380a97416a/skeleton.py
+++ exercises/fb508b79-1b5f-4f7c-889e-6dafa96bc3c6/skeletons/f0aaa6bb-aad4-4758-a1f2-6d380a97416a/skeleton.py
@@ -1,6 +1,8 @@
+# PLEASE DONT'T CHANGE THE w DICTIONARY
 w = {'energy emergency': 'pogotowie energetyczne', 'gas emergency': 'pogotowie gazowe', 'police': 'policja', 'fire brigade': u'straż pożarna', 'emergency': 'pogotowie ratunkowe'}
 
-print("Word?")
-word = input()
+
+print("Word?") # DISPLAY STRING 'Word?'  
+word = input() 
 
 print("False!")
